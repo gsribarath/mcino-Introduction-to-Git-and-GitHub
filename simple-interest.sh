35981593898390e0c3c7b04dcfb48987f5c3c424
Sri Barath G <gsribarath@gmail.com>
2026-02-06 10:01:32 +0530

    Refactor simple interest calculation script

--- simple-interest.sh
+++ simple-interest.sh
@@ -24,3 +24,18 @@ read t
 s=$(expr $p \* $t \* $r / 100)
 echo "The simple interest is: "
 echo $s
+#!/bin/bash
+
+echo "Enter Principal:"
+read p
+
+echo "Enter Rate of Interest:"
+read r
+
+echo "Enter Time Period (in years):"
+read t
+
+si=$(echo "$p * $r * $t / 100" | bc)
+
+echo "Simple Interest = $si"
+
